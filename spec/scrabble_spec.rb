faefa12d0ebb0c1d0dd711b08175ba7754df3583
Allison Mackey <a.mackey693@gmail.com>
2020-05-05 10:48:11 -0700

    run test on spec #2

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -5,4 +5,7 @@ describe ('#scrabble_score') do
   it("returns a scrabble score for a letter with a point value of 1") do
     expect(scrabble_score.fetch("a")).to(eq(1))
   end
+  it("returns a scrabble score for a letter with a point value of 2") do
+    expect(scrabble_score.fetch("d")).to(eq(2))
+  end
 end
