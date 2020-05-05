ae4960bc5d0b7096c180cf393aeae95e0ca580bb
Allison Mackey <a.mackey693@gmail.com>
2020-05-05 13:32:53 -0700

    finalize scrabble project. all tests pass and scrabble class is fully functional

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 require('pry') 
-
 class Scrabble 
   def initialize(word)
     @word = word 
@@ -21,8 +20,6 @@ class Scrabble
     points = { "a" => 1, "e" => 1, "i" => 1, "o" => 1, "u" => 1, "l" => 1, "n" => 1, "r" => 1,"s" => 1, "t" => 1, "d" => 2, "g" => 2, "b" => 3, "c" => 3, "m" => 3, "p" => 3, "f" => 4, "h" => 4, "v" => 4, "w" => 4, "y" => 4, "k" => 5, "j" => 8, "x" => 8, "q" => 10, "z" => 10 }
   end
 end 
-
-
 
 # notes: method to take the word and turn it into a numbers array
 # method to take the numbers array and add them together 
